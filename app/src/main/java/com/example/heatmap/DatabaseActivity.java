94e81fe0750deb4260bfdc86a032ecf6a9e6b429
Karkeys360 <Karkeys360@gmail.com>
2020-08-15 15:20:37 -0700

    Updated the DatabaseActivity class to be able to upload loaction of a user at a certain time

--- app/src/main/java/com/example/heatmap/DatabaseActivity.java
+++ app/src/main/java/com/example/heatmap/DatabaseActivity.java
@@ -1,4 +1,22 @@
 package com.example.heatmap;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+
+import java.util.*;
+
 public class DatabaseActivity {
+    private CollectionReference Users = FirebaseFirestore.getInstance().collection("users");
+
+    public void UploadInfo(String MacAdress, double longitude, double latitude, String Timestamp) {
+
+        GeoPoint Coordinates = new GeoPoint(latitude,longitude);
+        Map<String, Object> info = new HashMap<>();
+        info.put("lcoation",Coordinates);
+        info.put("Time",Timestamp);
+        Users.add(MacAdress);
+        Users.document(MacAdress).set(info);
+    }
 }
