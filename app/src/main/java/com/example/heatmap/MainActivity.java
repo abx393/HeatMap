0d9b8362b3b40ddbc73708c186ba5d649acb1352
abx393 <abandari99@gmail.com>
2020-08-15 13:22:38 -0700

    webview

--- app/src/main/java/com/example/heatmap/MainActivity.java
+++ app/src/main/java/com/example/heatmap/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.heatmap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        //myWebView.loadUrl("http://www.example.com");
     }
 }
