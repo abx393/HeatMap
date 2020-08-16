4e202098e609d1d304f2aa8e8336b14783e71e0d
abx393 <abandari99@gmail.com>
2020-08-15 17:42:38 -0700

    retrieving location data

--- app/src/main/java/com/example/heatmap/MainActivity.java
+++ app/src/main/java/com/example/heatmap/MainActivity.java
@@ -10,6 +10,8 @@ import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebView;
+import android.widget.TextView;
+
 import com.example.heatmap.DatabaseActivity;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     private static final int PERMISSION_REQUEST_LOCATION = 0;
     private FusedLocationProviderClient fusedLocationClient;
     private DatabaseActivity db;
+    private TextView latitudeText, longitudeText;
+    private boolean permissionGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,26 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         //(new DatabaseActivity()).uploadInfo("macaddress", 30.0, 30.0, "timestamp");
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+        latitudeText = findViewById(R.id.latitudeText);
+        longitudeText = findViewById(R.id.longitudeText);
+
+    }
+
+    public void getLocation() {
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            double latitude = location.getLatitude();
+                            double longitude = location.getLongitude();
+                            latitudeText.setText("latitude " + latitude);
+                            longitudeText.setText("longitude " + longitude);
+                            db = new DatabaseActivity();
+                            db.logLocation(latitude, longitude);
+                        }
+                    }
+                });
     }
 
     //@Override
@@ -40,32 +64,25 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         if (requestCode == PERMISSION_REQUEST_LOCATION) {
             if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Log.d("Yay", "Permission granted.");
-
-                fusedLocationClient.getLastLocation()
-                        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                            @Override
-                            public void onSuccess(Location location) {
-                                if (location != null) {
-                                    double latitude = location.getLatitude();
-                                    double longitude = location.getLongitude();
-                                    db = new DatabaseActivity();
-                                    db.logLocation(latitude, longitude);
-                                }
-                            }
-                        });
+                permissionGranted = true;
+                getLocation();
 
             } else {
+                permissionGranted = false;
+                Log.d("grantResults[0] " + grantResults[0], "PackageManager.PERMISSION_GRANTED" + PackageManager.PERMISSION_GRANTED);
                 Log.d("Fuck", "Permission denied.");
             }
         }
     }
 
     public void requestLocationPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Log.d("check 1", "check 1");
 
         } else {
+            Log.d("check 2", "check 2");
             ActivityCompat.requestPermissions(MainActivity.this,
-                                                new String[] {Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                                                new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
                                                 PERMISSION_REQUEST_LOCATION);
 
         }
