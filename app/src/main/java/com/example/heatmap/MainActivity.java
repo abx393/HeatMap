00d0dfc708f126d21d590d5fb34f060585146f17
abx393 <abandari99@gmail.com>
2020-08-15 23:10:15 -0700

    creating notifications

--- app/src/main/java/com/example/heatmap/MainActivity.java
+++ app/src/main/java/com/example/heatmap/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.heatmap;
 
 import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
@@ -13,6 +17,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -32,15 +38,18 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     private boolean permissionGranted = false;
     private WebView webview;
     private long updateInterval = 100000; //milliseconds
-
+    private String CHANNEL_ID = "channel_id";
+    private int notificationId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
+
+        createNotification();
 
         webview =(WebView)findViewById(R.id.webView);
-
         webview.setWebViewClient(new WebViewClient());
         webview.getSettings().setJavaScriptEnabled(true);
         webview.getSettings().setDomStorageEnabled(true);
@@ -70,6 +79,37 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         locationRequest.setInterval(updateInterval);
 
         requestLocationPermission();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence channelName = "channel0";
+            String channelDescription = "channel_description";
+            int channelImportance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, channelName, channelImportance);
+            channel.setDescription(channelDescription);
+
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+    public void createNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notif_icon)
+                .setContentTitle("Densely populated area!")
+                .setContentText("Lots of people around you! Please wear your mask.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(notificationId, builder.build());
+        notificationId++;
     }
 
     public void getLocation() {
