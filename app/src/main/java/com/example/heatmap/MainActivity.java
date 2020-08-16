62a4222d7ce5d19dabda091de6c3dc027e4610d6
abx393 <abandari99@gmail.com>
2020-08-15 18:50:19 -0700

    location updates at regular intervals

--- app/src/main/java/com/example/heatmap/MainActivity.java
+++ app/src/main/java/com/example/heatmap/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,6 +15,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
@@ -21,10 +25,14 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
 
     private static final int PERMISSION_REQUEST_LOCATION = 0;
     private FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
     private DataBase db;
     private TextView latitudeText, longitudeText;
     private boolean permissionGranted = false;
     private WebView webview;
+    private long updateInterval = 10000; //milliseconds
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +47,29 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
         webview.loadUrl("https://www.youtube.com");
 
-
-        requestLocationPermission();
         //(new DatabaseActivity()).uploadInfo("macaddress", 30.0, 30.0, "timestamp");
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         latitudeText = findViewById(R.id.latitudeText);
         longitudeText = findViewById(R.id.longitudeText);
 
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) return;
+                for (Location location : locationResult.getLocations()) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    latitudeText.setText("latitude " + latitude);
+                    longitudeText.setText("longitude " + longitude);
+                    DataBase.logLocation(latitude, longitude);
+                }
+            }
+        };
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(updateInterval);
+
+        requestLocationPermission();
     }
 
     public void getLocation() {
@@ -66,13 +89,18 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
                 });
     }
 
+    public void startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+    }
+
     //@Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == PERMISSION_REQUEST_LOCATION) {
             if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Log.d("Yay", "Permission granted.");
                 permissionGranted = true;
-                getLocation();
+                //getLocation();
+                startLocationUpdates();
 
             } else {
                 permissionGranted = false;
@@ -83,14 +111,17 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     }
 
     public void requestLocationPermission() {
+
+        Log.d("check 2", "check 2");
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[] {Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                PERMISSION_REQUEST_LOCATION);
+
         if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
             Log.d("check 1", "check 1");
 
         } else {
-            Log.d("check 2", "check 2");
-            ActivityCompat.requestPermissions(MainActivity.this,
-                                                new String[] {Manifest.permission.ACCESS_BACKGROUND_LOCATION},
-                                                PERMISSION_REQUEST_LOCATION);
+
 
         }
     }
